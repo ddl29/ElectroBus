82326b978ddd7be4e1a8f63eb2f2dec7b39cd6cd
dmacias77 <dmacias77@outlook.com>
2022-06-13 12:33:53 -0500

    Standardization, language adaptation & redesign

--- app/src/main/java/com/example/electrobus/User.kt
+++ app/src/main/java/com/example/electrobus/User.kt
@@ -1,10 +1,10 @@
 package com.example.electrobus
 
 class User(val name: String,
-           val matricula: String,
+           val id: String,
            val email: String,
            val password: String,
-           val typeUser: String) {
+           val typeUser: String,
+           val mainRoute: String) {
 
-
-}
+           }
